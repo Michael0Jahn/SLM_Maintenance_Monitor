0c58bf9dd6e9c88350a614fccd42c3ad8a3a27cc
Michael0Jahn <ic21b029@technikum-wien.at>
2022-12-31 12:00:27 +0100

    Initial commit

---
+++ src/main/java/at/hftw/slm_maintenance_monitor/SlmMaintenanceMonitorApplication.java
@@ -0,0 +1,13 @@
+package at.hftw.slm_maintenance_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmMaintenanceMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmMaintenanceMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/hftw/slm_maintenance_monitor/SlmMaintenanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.hftw.slm_maintenance_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmMaintenanceMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
