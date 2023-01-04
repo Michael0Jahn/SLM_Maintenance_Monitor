68b001842891d7e3073726bbe1bd5d25e51ae765
Pepiljo <knezev13@gmail.com>
2023-01-04 17:52:01 +0100

    Added methods which set message, reset message, default message and reads out current message

--- src/main/java/at/hftw/slm_maintenance_monitor/controller/MaintenanceController.java
+++ src/main/java/at/hftw/slm_maintenance_monitor/controller/MaintenanceController.java
@@ -1,7 +1,25 @@
 package at.hftw.slm_maintenance_monitor.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MaintenanceController {
+    private static String message = "";
+
+    @GetMapping("/")
+    public String Message(){
+        return message;
+    }
+    @GetMapping("/status/default")
+    public String defaultMessage(){
+        message = "Everything operates as expected";
+        return message;
+    }
+
+    @GetMapping("/status")
+    public String serviceMessage(String newMessage){
+        message = newMessage;
+        return message;
+    }
 }
